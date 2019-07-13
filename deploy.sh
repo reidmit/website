420e98844118e3cd8830fd7ca7e85df665cfc0b1
Reid Mitchell <reidmitchell1@gmail.com>
2019-07-13 16:13:37 -0700

    Improve deploy script again

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,17 @@ rm -rf ./public/**
 echo "Building site..."
 hugo
 
+echo "Staging ./public changes..."
+git add public
+
+currentDate=`date`
+git ci -m "Deploy! ($currentDate)"
+
 echo "Pushing ./public to gh-pages branch..."
 git subtree push --prefix public origin gh-pages
+
+echo "Undoing deploy commit..."
+git reset HEAD^
 
 echo "Cleaning out ./public directory again..."
 rm -rf ./public/**
